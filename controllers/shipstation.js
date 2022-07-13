7d3caa41bfad33cba601a28b6feb3b5ff300961b
Meet Thakkar <meetthakker1@gmail.com>
2022-07-13 12:37:39 +0530

    code cleanup

--- controllers/shipstation.js
+++ controllers/shipstation.js
@@ -18,10 +18,12 @@ exports.newOrders = async (req, res, next) => {
 
       // skip splittig order that are already splitted 
       let filterOrders = response.data.orders.filter((order) => !order.orderNumber.toLowerCase().includes('-'));
+
       let splitOrders = [];
       // if only one type of sku in order then do not split
+
       filterOrders.forEach((order) => {
-        const distinctSKUs = order.items.map((item) => {
+        let distinctSKUs = order.items.map((item) => {
           if (item.sku.toLowerCase().includes('cb1')) {
             return 'cb1';
           } else if (item.sku.toLowerCase().includes('cb3')) {
@@ -36,6 +38,7 @@ exports.newOrders = async (req, res, next) => {
             return item.sku.toLowerCase();
           }
         });
+        distinctSKUs = [...new Set(distinctSKUs)];
         console.log('distinctSKUs>>>>', distinctSKUs)
         if (distinctSKUs.length === 1 && SKUs.indexOf(distinctSKUs[0]) >= 0) {
           // do nothing
@@ -74,17 +77,6 @@ const analyzeOrders = async (newOrders) => {
     try {
       const order = newOrders[x];
 
-      // Create an array of all the individual warehouseLocations present on the order.
-      // const warehouses = [
-      //   ...new Set(
-      //     order.items.map((item) => {
-      //       if (item.warehouseLocation != null) {
-      //         return item.warehouseLocation;
-      //       }
-      //     })
-      //   ),
-      // ];
-
       // CB1 or CB3 or CB6 or ESSENTIALS
       const SKUs = ['cb1', 'cb3', 'cb6', 'essentials'];
       const itemSKUs = [];
@@ -93,17 +85,6 @@ const analyzeOrders = async (newOrders) => {
           itemSKUs.push(SKU);
         }
       });
-      // const findItemSKUs = order.item.find((item) => item.sku.toLowerCase().includes("CB1") || item.sku.toLowerCase().includes("CB3") || item.sku.toLowerCase().includes("CB6") || item.sku.toLowerCase().includes("ESSENTIALS"))
-
-      // If there are multiple warehouse locations, split the order.
-      // if (warehouses.length > 1) {
-      //   const orderUpdateArray = splitShipstationOrder(order, warehouses);
-      //   await shipstationApiCall(
-      //     "https://ssapi.shipstation.com/orders/createorders",
-      //     "post",
-      //     orderUpdateArray
-      //   );
-      // }
 
       if (itemSKUs.length > 0) {
         const orderUpdateArray = await splitShipstationOrder(order, itemSKUs);
@@ -121,10 +102,10 @@ const analyzeOrders = async (newOrders) => {
 
 /**
  * Copies the primary order for each new order, adjusting the items on each to correspond
- * to the correct warehouse location.
+ * to the correct SKU.
  *
  * @param  {object} order an order object from the ShipStation API
- * @param {array} warehouses an array of strings containing the warehouse names
+ * @param {array} SKUs an array of strings containing the SKU names
  *
  * @return {array} an array of order objects to be updated in ShipStation
  */
@@ -139,30 +120,23 @@ const splitShipstationOrder = async (order, SKUs) => {
     orderUpdateArray.push(mainOrder);
   }
   let updatedMainOrder = false;
-  // Loop through every warehouse present on the order.
+  // Loop through every SKU present on the order.
   for (let x = 0; x < SKUs.length; x++) {
     try {
       // Create a copy of the original order object.
       let tempOrder = { ...order };
 
-      // Give the new order a number to include the warehouse as a suffix.
+      // Give the new order a number to include the SKU as a suffix.
       tempOrder.orderNumber = `${tempOrder.orderNumber}-${SKUs[x]}`;
 
-      // Filter for the order items for this specific warehouse.
+      // Filter for the order items for this specific SKU.
       tempOrder.items = tempOrder.items.filter((item) => {
-        // If the item's warehouseLocation is null, assign it to the first warehouse present.
-        // if (item.warehouseLocation == null && x === 0) {
-        //   item.warehouseLocation = SKUs[x];
-        // }
-        // return item.warehouseLocation === SKUs[x];
         return item.sku != null && item.sku.toLowerCase().includes(SKUs[x]);
       });
 
       console.log(`tempOrder.items for ${SKUs[x]}`, tempOrder.orderNumber, tempOrder.items);
 
       // If this is not the first (primary) order, set the object to create new order in ShipStation.
-      // if (mainOrder.items.length == 0) {
-      // }
       if (mainOrder.items.length === 0 && !updatedMainOrder) {
         updatedMainOrder = true;
       } else {
